cb104bf4a860563039af653b91e7d79b48927a5f
Mario Gellrich <mario.gellrich@gmx.ch>
2024-09-19 16:12:30 +0200

    ...

--- init-db.sql
+++ init-db.sql
@@ -1,9 +1,2 @@
 -- init-db.sql
 CREATE ROLE postgres WITH LOGIN PASSWORD 'geheim' SUPERUSER;
-
--- Create a new (empty) database named 'osm_switzerland'
-CREATE DATABASE osm_switzerland;
-
--- Connect to the 'osm_switzerland' database and create the PostGIS extension
-\connect osm_switzerland
-CREATE EXTENSION postgis;
