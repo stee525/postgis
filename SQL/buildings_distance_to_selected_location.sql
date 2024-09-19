b440787a0977fcca8c0fe9fbfd1354bdb36e5e80
mario-gellrich-zhaw <60516564+mario-gellrich-zhaw@users.noreply.github.com>
2024-09-19 14:50:58 +0000

    ...

--- SQL/buildings_distance_to_selected_location.sql
+++ SQL/buildings_distance_to_selected_location.sql
@@ -5,12 +5,12 @@ SELECT
   p."addr:city",
   p."addr:postcode",
   p.building,
-  ST_Transform(p.way, 4326) AS way_transformed,
   ST_Distance(
     ST_Transform(p.way, 4326)::geography,
   -- Zürich, Grossmünster
     ST_SetSRID(ST_MakePoint(8.54394, 47.37011), 4326)::geography
-  ) AS distance
+  ) AS distance,
+  ST_Transform(p.way, 4326) AS way_transformed
 FROM 
   public.planet_osm_polygon AS p
 WHERE 
